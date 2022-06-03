7f782512a6aa1846eb60acfc01096273fb218d9f
Régis Behmo <regis@behmo.com>
2022-06-03 13:07:01 +0200

    v14.0.0: upgrade to Nutmeg

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     packages=find_packages(exclude=["tests*"]),
     include_package_data=True,
     python_requires=">=3.5",
-    install_requires=["tutor>=13.2.0,<14.0.0", "click_repl>=0.2.0"],
+    install_requires=["tutor>=14.0.0,<15.0.0", "click_repl>=0.2.0"],
     entry_points={
         "tutor.plugin.v1": ["webui = tutorwebui.plugin"],
     },
