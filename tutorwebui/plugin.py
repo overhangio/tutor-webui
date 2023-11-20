998de5eb006fe9471d6e78e7cca9bbb8fe71373e
Régis Behmo <regis@behmo.com>
2023-11-20 17:39:47 +0100

    feat: simplify nightly version management
    By pulling the version suffix from tutor, we avoid git conflicts when
    merging the release branch in nightly.

--- tutorwebui/plugin.py
+++ tutorwebui/plugin.py
@@ -1,6 +1,11 @@
 from tutor import hooks as tutor_hooks
+from tutor.__about__ import __version_suffix__
 from .__about__ import __version__
 from . import cli
+
+# Handle version suffix in nightly mode, just like tutor core
+if __version_suffix__:
+    __version__ += "-" + __version_suffix__
 
 tutor_hooks.Filters.CLI_COMMANDS.add_items(
     [
